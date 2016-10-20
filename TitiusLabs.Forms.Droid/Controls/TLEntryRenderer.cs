8cf8950e2e41360c45e6124033b39065f36b0326
fabiocozzolino <fabio@MacBook-Air-di-FabioMac.local>
2016-10-20 23:04:40 +0200

    added android version

---
+++ TitiusLabs.Forms.Droid/Controls/TLEntryRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TitiusLabs.Forms.Controls;
+using TitiusLabs.Forms.Droid.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(TLEntry), typeof(TLEntryRenderer))]
+
+namespace TitiusLabs.Forms.Droid.Controls
+{
+	public class TLEntryRenderer : EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			var element = e.NewElement as TLEntry;
+			if (element.ReturnButton == ReturnButtonType.Next)
+			{
+				Control.ImeOptions = Android.Views.InputMethods.ImeAction.Next;
+				Control.EditorAction += (sender, args) =>
+				{
+					element.OnNext();
+				};
+			}
+		}
+	}
+}
