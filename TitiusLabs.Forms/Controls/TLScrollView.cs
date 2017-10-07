f539bfd8e2ed55ce2552f532f9d4b6be31dd9627
fabiocozzolino <fabio@MacBook-Air-di-FabioMac.local>
2017-10-07 16:11:52 +0200

    correct format

--- TitiusLabs.Forms/Controls/TLScrollView.cs
+++ TitiusLabs.Forms/Controls/TLScrollView.cs
@@ -10,10 +10,10 @@ using System.Linq;
 
 namespace TitiusLabs.Forms.Controls
 {
-	public class TLScrollView : ScrollView
-	{
-		public static readonly BindableProperty ItemsSourceProperty =
-            BindableProperty.Create("ItemsSource", typeof(IEnumerable), typeof(TLScrollView), default(IEnumerable), 
+    public class TLScrollView : ScrollView
+    {
+        public static readonly BindableProperty ItemsSourceProperty =
+            BindableProperty.Create("ItemsSource", typeof(IEnumerable), typeof(TLScrollView), default(IEnumerable),
                                     BindingMode.Default, null, new BindableProperty.BindingPropertyChangedDelegate(HandleBindingPropertyChangedDelegate));
 
         private static object HandleBindingPropertyChangedDelegate(BindableObject bindable, object value)
@@ -22,55 +22,55 @@ namespace TitiusLabs.Forms.Controls
         }
 
         public IEnumerable ItemsSource
-		{
-			get { return (IEnumerable)GetValue(ItemsSourceProperty); }
-			set { SetValue(ItemsSourceProperty, value); }
-		}
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
 
-		public static readonly BindableProperty ItemTemplateProperty =
-			BindableProperty.Create("ItemTemplate", typeof(DataTemplate), typeof(TLScrollView), default(DataTemplate));
+        public static readonly BindableProperty ItemTemplateProperty =
+            BindableProperty.Create("ItemTemplate", typeof(DataTemplate), typeof(TLScrollView), default(DataTemplate));
 
-		public DataTemplate ItemTemplate
-		{
-			get { return (DataTemplate)GetValue(ItemTemplateProperty); }
-			set { SetValue(ItemTemplateProperty, value); }
-		}
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
 
-		public event EventHandler<ItemTappedEventArgs> ItemSelected;
+        public event EventHandler<ItemTappedEventArgs> ItemSelected;
 
-		public static readonly BindableProperty SelectedCommandProperty =
-			BindableProperty.Create("SelectedCommand", typeof(ICommand), typeof(TLScrollView), null);
+        public static readonly BindableProperty SelectedCommandProperty =
+            BindableProperty.Create("SelectedCommand", typeof(ICommand), typeof(TLScrollView), null);
 
-		public ICommand SelectedCommand
-		{
-			get { return (ICommand)GetValue(SelectedCommandProperty); }
-			set { SetValue(SelectedCommandProperty, value); }
-		}
+        public ICommand SelectedCommand
+        {
+            get { return (ICommand)GetValue(SelectedCommandProperty); }
+            set { SetValue(SelectedCommandProperty, value); }
+        }
 
-		public static readonly BindableProperty SelectedCommandParameterProperty =
-			BindableProperty.Create("SelectedCommandParameter", typeof(object), typeof(TLScrollView), null);
+        public static readonly BindableProperty SelectedCommandParameterProperty =
+            BindableProperty.Create("SelectedCommandParameter", typeof(object), typeof(TLScrollView), null);
 
-		public object SelectedCommandParameter
-		{
-			get { return GetValue(SelectedCommandParameterProperty); }
-			set { SetValue(SelectedCommandParameterProperty, value); }
-		}
+        public object SelectedCommandParameter
+        {
+            get { return GetValue(SelectedCommandParameterProperty); }
+            set { SetValue(SelectedCommandParameterProperty, value); }
+        }
 
         static void HandleBindingPropertyChangedDelegate(BindableObject bindable, object oldValue, object newValue)
         {
-			var isOldObservable = oldValue?.GetType().GetTypeInfo().ImplementedInterfaces.Any(i => i == typeof(INotifyCollectionChanged));
-			var isNewObservable = newValue?.GetType().GetTypeInfo().ImplementedInterfaces.Any(i => i == typeof(INotifyCollectionChanged));
+            var isOldObservable = oldValue?.GetType().GetTypeInfo().ImplementedInterfaces.Any(i => i == typeof(INotifyCollectionChanged));
+            var isNewObservable = newValue?.GetType().GetTypeInfo().ImplementedInterfaces.Any(i => i == typeof(INotifyCollectionChanged));
 
-			var tl = (TLScrollView)bindable;
-			if (isOldObservable.GetValueOrDefault(false))
+            var tl = (TLScrollView)bindable;
+            if (isOldObservable.GetValueOrDefault(false))
             {
                 ((INotifyCollectionChanged)oldValue).CollectionChanged -= tl.HandleCollectionChanged;
-			}
+            }
 
-			if (isNewObservable.GetValueOrDefault(false))
-			{
-				((INotifyCollectionChanged)newValue).CollectionChanged += tl.HandleCollectionChanged;
-			}
+            if (isNewObservable.GetValueOrDefault(false))
+            {
+                ((INotifyCollectionChanged)newValue).CollectionChanged += tl.HandleCollectionChanged;
+            }
 
             if (oldValue != newValue)
             {
@@ -84,38 +84,38 @@ namespace TitiusLabs.Forms.Controls
         }
 
         public void Render()
-		{
+        {
             if (ItemTemplate == null || ItemsSource == null)
             {
                 Content = null;
                 return;
             }
 
-			var layout = new StackLayout();
-			layout.Orientation = Orientation == ScrollOrientation.Vertical ? StackOrientation.Vertical : StackOrientation.Horizontal;
+            var layout = new StackLayout();
+            layout.Orientation = Orientation == ScrollOrientation.Vertical ? StackOrientation.Vertical : StackOrientation.Horizontal;
 
-			foreach (var item in ItemsSource)
-			{
-				var command = SelectedCommand ?? new Command((obj) =>
-				{
-					var args = new ItemTappedEventArgs(ItemsSource, item);
-					ItemSelected?.Invoke(this, args);
-				});
-				var commandParameter = SelectedCommandParameter ?? item;
+            foreach (var item in ItemsSource)
+            {
+                var command = SelectedCommand ?? new Command((obj) =>
+                {
+                    var args = new ItemTappedEventArgs(ItemsSource, item);
+                    ItemSelected?.Invoke(this, args);
+                });
+                var commandParameter = SelectedCommandParameter ?? item;
 
-				var viewCell = ItemTemplate.CreateContent() as ViewCell;
-				viewCell.View.BindingContext = item;
-				viewCell.View.GestureRecognizers.Add(new TapGestureRecognizer
-				{
-					Command = command,
-					CommandParameter = commandParameter,
-					NumberOfTapsRequired = 1
-				});
+                var viewCell = ItemTemplate.CreateContent() as ViewCell;
+                viewCell.View.BindingContext = item;
+                viewCell.View.GestureRecognizers.Add(new TapGestureRecognizer
+                {
+                    Command = command,
+                    CommandParameter = commandParameter,
+                    NumberOfTapsRequired = 1
+                });
 
-				layout.Children.Add(viewCell.View);
-			}
+                layout.Children.Add(viewCell.View);
+            }
 
-			Content = layout;
-		}
-	}
+            Content = layout;
+        }
+    }
 }
